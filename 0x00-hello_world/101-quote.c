39d4623068877adb55af368b8f6ca29b580ed632
martinrmbugua <mailto:martinrmbugua@gmail.com>
2022-09-08 19:17:30 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - print quote
+*
+* Description: prints Dora's quote
+*
+* Return: Always 1 (success)
+*/
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+return (1);
+}
