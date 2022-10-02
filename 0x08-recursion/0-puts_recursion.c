96d545c38d39ed81720848e3ccf42e1024c2f279
martinrmbugua <mailto:martinrmbugua@gmail.com>
2022-10-02 00:26:06 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,19 +5,13 @@
  *  @s: string
  *  Return: no return.
  */
-
 void _puts_recursion(char *s)
 {
-		if (*s != '\0')
-				{
-
-					_putchar(*s);
-					_puts_recursion(s + 1);
-
-				}
-
-		else
-
-			_putchar('\n');
-
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	_putchar('\n');
 }
