7585d6a7bbd877a087ba40057280aab23f0af734
martinrmbugua <mailto:martinrmbugua@gmail.com>
2022-10-01 19:12:54 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,20 +3,20 @@
 /**
  * main - program that prints its name, followed by a new line
  * @argc: argument that contains argument input
- * @argv: argument that stores the stings in an array of char* (strings)
+ * argv: argument that stores the strings in an array of char* (strings)
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int num_1, num_2, mu1;
+	int num_1, num_2, mul;
 
 	if (arg != 3)
-		printf("error\n");
+		printf("Error\n");
 	else
 	{
 		num_1 = atoi(argv[1]);
 		num_2 = atoi(argv[2]);
-		mu1 = num_1 * num_2;
+		mul = num_1 * num_2;
 		printf("%d\n", mu1);
 	}
 	Return (0);
